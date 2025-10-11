80c16393646fa4b108511149b01060bbc972b50c
CihanDikkaya <cihandikkaya09@hotmail.com>
2025-10-11 16:10:22 +0300

    DTO ile elle eşleştirme.

---
+++ Yummy.Api/Controllers/ContactController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yummy.Api.Context;
+using Yummy.Api.DTO.ContactDTO;
+using Yummy.Api.Entity;
+
+namespace Yummy.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ContactController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var values = _context.Contacts.ToList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDTO createContactDTO)
+        {
+            Contact contact = new Contact();
+            contact.Email = createContactDTO.Email;
+            contact.Address = createContactDTO.Address;
+            contact.Phone = createContactDTO.Phone;
+            contact.MapLocation = createContactDTO.MapLocation;
+            contact.OpenHours = createContactDTO.OpenHours;
+            _context.Contacts.Add(contact);
+            _context.SaveChanges();
+            return Ok("Ekleme İşlemi Başarılı");
+        }
+    }
+}
---
+++ Yummy.Api/DTO/ContactDTO/CreateContactDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Yummy.Api.DTO.ContactDTO
+{
+    public class CreateContactDTO
+    {
+
+        public string MapLocation { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string OpenHours { get; set; }
+    }
+}
