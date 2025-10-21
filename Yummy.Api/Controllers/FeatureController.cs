a9508da355555d1c769f07a841e4ae73884d3d2c
CihanDikkaya <cihandikkaya09@hotmail.com>
2025-10-21 15:11:19 +0300

    Feature sınıfı için ekleme silme güncelleme ve veri getirme.

---
+++ Yummy.Api/Controllers/FeatureController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yummy.Api.Context;
+using Yummy.Api.DTO.FeatureDTO;
+using Yummy.Api.Entity;
+
+namespace Yummy.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ApiContext _context;
+
+        public FeatureController(IMapper mapper, ApiContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var values = _context.Features.ToList();
+            return Ok(_mapper.Map<List<ResultFeatureDTO>>(values));
+        }
+
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDTO createFeatureDTO)
+        {
+            var value = _mapper.Map<Feature>(createFeatureDTO);
+            _context.Features.Add(value);
+            _context.SaveChanges();
+            return Ok("Ekleme Başarılı");
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            _context.Features.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme başarılı");
+        }
+
+        [HttpGet("GetFeature")]
+        public IActionResult GetFeature(int id)
+        {
+            var values = _context.Features.Find(id);
+            return Ok(_mapper.Map<GetByIDFeatureDTO>(values));
+        }
+
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateFeatureDTO updateFeatureDTO)
+        {
+            var values = _mapper.Map<Feature>(updateFeatureDTO);
+            _context.Features.Update(values);
+            _context.SaveChanges();
+            return Ok("Güncelleme Başarılı");
+
+        }
+    }
+}
