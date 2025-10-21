b0a216bf2db0ed66c798891174fcfd4932f1c570
CihanDikkaya <cihandikkaya09@hotmail.com>
2025-10-21 16:07:04 +0300

    Fluent Validation kütüphanesi ile veri girerken belirli kurallar girildi.

---
+++ Yummy.Api/ValidationRules/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Yummy.Api.Entity;
+
+namespace Yummy.Api.ValidationRules
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ürün adını boş geçmeyin.");
+            RuleFor(p => p.Name).MinimumLength(2).WithMessage("En az 2 karakter girişi yapın!");
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage("En fazla 50 karakter girişi yapın!");
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Ürün fiyatı boş geçilemez!")
+                .LessThan(0).WithMessage("Ürün fiyatı 0 dan aşağı olamaz!").GreaterThan(1000).WithMessage("Ürün fiyatı bu kadar yüksek olamaz, girdiğiniz değeri kontrol edin!");
+
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Ürün açıklaması boş geçilemez!");
+        }
+    }
+}
