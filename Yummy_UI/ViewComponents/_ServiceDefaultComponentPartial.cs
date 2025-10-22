be39c40fd52dbad297f3a805af4514eb456cd0c4
CihanDikkaya <cihandikkaya09@hotmail.com>
2025-10-22 10:11:15 +0300

    View Componentler bitimi.

---
+++ Yummy_UI/ViewComponents/_ServiceDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yummy_UI.ViewComponents
+{
+    public class _ServiceDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
